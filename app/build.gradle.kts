3e143d12215852d7b55e411724fd811776ff7b9a
Rémi Marseault <larsluph@larsluph.dev>
2026-07-05 10:00:23 +0200

    Bump to 1.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ extensions.configure<ApplicationExtension> {
         applicationId = "com.larsluph.singlecounter"
         minSdk = 33
         targetSdk = 37
-        versionCode = 5
-        versionName = "1.3.1"
+        versionCode = 6
+        versionName = "1.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
